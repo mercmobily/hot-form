e782eff1760a87bff9823e2f4cfd3620837e0191
Tony Mobily <tonymobily@gmail.com>
2016-07-18 23:56:39 +0800

    Added script to make up README.md

---
+++ make_readme.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IFS=''
+active='0';
+cat  hot-form-validator.html | while read "line";do
+
+
+  echo $line | grep '\-\->' > /dev/null
+  if [ $active = '1' -a $? = '0' ];then 
+    exit 0;
+  fi;
+
+  suppress=0;
+  echo $line | grep '^ *@' > /dev/null
+  if [ $? = '0' ];then suppress='1'; fi;
+  
+
+  if [ $active = '1' -a $suppress = '0' ];then echo $line;fi;
+
+  echo $line | grep "<!--" > /dev/null
+  if [ $? = '0' ];then active='1'; fi;
+
+done
